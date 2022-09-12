0fb644aa797c3243c6473faee80ca52259631129
Gabriel_Loredo <109186871+gabrielloredo710@users.noreply.github.com>
2022-09-12 13:16:01 -0300

    Famílias parte 1

---
+++ html/data/familias.js
@@ -0,0 +1,92 @@
+var familias = [
+    {
+       nome:"Malfoy",
+       descricao:"Família antiga, descendente do Grande bruxo Montague Knightley morto em 1702. São muito ricos, devido á fortuna feita por Nicholas Malfoy com a grande loja trevista Malfoy's Virtú, e pela outra fortuna formada por Brutus Malfoy com a Feitiçaria Aguerrida e com uma rede de lojas diversas. São influentes, e utilizam do dinheiro para conseguir o que desejam. Tiveram, desde sua fundação, ligações secretas com as artes-das-trevas, e utilizaram delas em muitas eras, de muitas maneiras. Prezam os valores de Sangues-Puro e sentem repulsa por mestiços e trouxas. Atualmente seu líder é Lúcio Malfoy, e a família Malfoy é a mais rica do Banco Gringott's.",
+       brasao:"../img/familias/malfoy."
+    },
+    {
+        nome:"Potter",
+        descricao:"Ignoto Peverell, o legitímo ancestral, era modesto e humilde. Quando assumiu uma idade avançada, deu a Capa da Invisibilidade que arrebatara da própria morte ao seu filho. Acolheu a morte como uma velha amiga, e a acompanhou de bom grado.",
+        brasao:"../img/familias/",
+     },
+     {
+        nome:"Weasley",
+        descricao:"São uma família de fortes características e crenças incontestáveis, os Weasley são o correspondente a uma enorme família de bruxos, que mesmo atualmente em uma boa condição moral e financeira, mantem seus conceitos a respeito de tudo que acredita.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Harris",
+        descricao:"Harris era um nome de uma família trouxa, comum, residente em Londres até ao dia que John Harris decide casar com Rose Larroquette, uma bruxa de renome, jogadora de quadribol. A partir de aí a família Harris passou a ter magia nas suas veias. Deles nasceu um único filho, Liam Harris, um bruxo. Agora Liam está encarregue da família enquanto que seu pai e sua mãe estão vivendo na Austrália, vivendo a boa vida da reforma.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Greengrass",
+        descricao:"Família sangue-puro, geralmente Sonserinos. São belos, arrogantes e profundamente orgulhosos de quem são. Possui membros nobres como Daphnee Greengrass Montague e Astoria Greengrass Malfoy.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Shacklebolt",
+        descricao:"Fundada por Roberto Shack e Sarah Le Bolt, a família tem como principais características a humildade, lealdade, coragem e sabedoria. Uma família extremamente habilidosa e experiente em duelos e Defesa Contra as Artes das Trevas.Geralmente são bruxos altos, bem constituídos. São todos de raça negra e sangue-puro.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Godunov",
+        descricao:"Fundada por Boris e Flora a Família Godunov há anos mantém um linhagem de sangue-puro e exemplos clássicos de inteligência e sabedoria por parte de seus membros. São naturalmente da Rússia e quase todos os membros possuem uma massa corporal avantajada por serem em sua grande maioria alunos do Instituto Durmstrang de Magia.  Seu herdeiro é Mathias Godunov filho de Boris e Flora; O mesmo atualmente leciona a matéria da História da Magia atuando também como diretor da casa de Haus Wasser que por coincidência surpreendente tem sido há anos a comunal dos Godunov's.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Brunner",
+        descricao:"Fundada por volta do século XI, a família é originaria da Austália.Seu início se teve por Bernard Lawrence Brunner I, um bruxo muito respeitado de sua época que tornou-se Ministro da Magia da Austrália aos 40 anos. É uma família de nobreza exemplar, sangue-puro e rica. É uma família influente no mundo bruxo, e utilizam de sua riqueza para ter o que querem.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Diggory",
+        descricao: "Habitam uma longínqua Mansão, em Hogsmeade. São de puro-sangue, geralmente lufinos ou corvinos. Diggory, um requisito cheio de regras. Marcantes por sua repugnância aos considereados desprezíveis. Legítmos pela cede por justiça. Características únicas da família. Renda de Classe-Média. Descendentes de Eldricht Diggory. Abreviando, fascinados pela família em que nasceram.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Mc Gonagall",
+        descricao:"Uma das famílias mais antigas que existe, sua tradição vem de origem escocesa. Familia de sangue puro com uma grande rigidez com relação às regras, esperteza e inteligência.",
+        brasao:"../img/familias/"
+     },
+     {
+        nome:"Scamander",
+        descricao:"A família Scamander é uma família de sangue puro, que tem um grande  membro , Newt Scamander. Ele escreveu o livro ''Animais Fantásticos e Onde Habitam''. A familía também é conhecida por Luna Lovegood, que se casou com Rolf Scamander e teve dois gemeos,  Lysander e Lorcan. Os gemêos Lysander e Lorcan atualmente estão na Escola de Magia e Bruxaria de Hogwarts.",
+        brasao:"../img/familias/"
+     },    
+     {
+        nome:"Astor",
+        descricao:"A família Astor foi fundada em 1912, por Thomas Astor. Apesar de ser recente, sempre foi e continua com sangue puro. Thomas era um homem bem rico, e ao morrer deixou sua grande herança para seu filho, John Astor. Esse fez o mesmo para Charles Astor e Benjamin Astor, e será assim até o fim dos dias. Não possuem preconceito contra não-puros, mas sentem certa repulsa por bruxos pobres ou lunáticos. São políticos natos, e também muito habilidosos na alquimia e nas poções mágicas.",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Black",
+        descricao:"Seus membros são todos sangues-puros. Não aceitam parentescos trouxas ou mestiços, além de Sirius Black. Sirius discorda dessa teoria por pensar que trouxas e mestiços também podem frequentar o mundo bruxo.Alguns membros são trevistas habilidosos, famosos e ardilosos.E uma das Familias mais Ricas de toda Hogwarts(RPG).",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Graff VilleFort",
+        descricao:"A Família Graff VilleFort e uma das Famílias mais leias, Destemidas, de Puro-Sangue, a Família Graff VilleFort foi constituída por Edward Graff VilleFort, A Família Graff VilleFort e uma família de vários costumes, uma família estudiosa, leal, humilde, E alegre, sempre ajuda uns aos outros pelo seu temperamento alegre, a família Graff VilleFort vem de costumeis Inglesas, Com isso em Hogwarts casa de costume é a Gryffindor, mas a alguns familiares de outras casas menos a Slytherin Hoje em dia a Família Graff VilleFort vive em Godric's Hollow, e ainda segue todos os costumes de antigamente.",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Lannister",
+        descricao:"Bulfrick Lannister fundara a família a mais de mil anos. Leais as Trevas e adoradores do purismo sanguíneo, prezam tal, que na idade média chegaram a realizar casamentos entre primos para preservar o sangue. São desprezados pela maioria dos mestiços e nascidos trouxas, por causa de seu grande preconceito. A família pertence a Casa da Sonserina, pois são astutos e são sedentos de poder. O Castelo dos mesmos fica na Albânia, em um vilarejo coberto por uma neblina espessa que nunca se extingue. E agora possuem uma Mansão no Número Treze, na Rua Knights Bridge, em Londres.",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Montague",
+        descricao:"Os Montague são uma nobre família bruxa, muito conhecida por todo o mundo bruxo, especialmente pelo seu caráter duvidoso e sua profunda repulsa por pessoas que não tem descendência completamente mágica, tem grande parte dos membros mortos.",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Wood",
+        descricao:"Familia tradicional bruxa, e famosa por ser descendente dos bruxos Dexter Fortescue e Rodrigo Plumpton. Local de origem desconhecido, porém seus atuais herdeiros vivem em um chalé localizado em Hogsmead. A história da familia começa quando seus herdeiros partem em busca de novas experiências em Hogwarts. Seu membro mais famoso é o ex-Capitão do time de Quadribol da Grifinória Olivio Wood, que hoje é Goleiro do Time Puddlemere.",
+        brasao:"../img/familias/"
+     },     
+     {
+        nome:"Lupin",
+        descricao:"Uma família de grandes bruxos , não muito antiga, com muitas histórias contadas e para ainda serem contadas.Familia Lupin muito importante no mundo do bruxo.",
+        brasao:"../img/familias/"
+     },
+]
