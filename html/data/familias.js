21fef2a8f30aad59dac9425d2f17b2ed6e9e0413
Masso <111982693+Kamylle21@users.noreply.github.com>
2022-09-13 15:58:23 -0300

    Update familias.js
    Parte final dos dados das familias

--- html/data/familias.js
+++ html/data/familias.js
@@ -89,4 +89,166 @@ var familias = [
         descricao:"Uma família de grandes bruxos , não muito antiga, com muitas histórias contadas e para ainda serem contadas.Familia Lupin muito importante no mundo do bruxo.",
         brasao:"../img/familias/"
      },
+     {
+      nome:"PEVERELL",
+      descricao:"Muito antiga e nobre família bruxa. Os Peverell são popularmente conhecidos devido ao Conto dos Três Irmãos. De fato, os primeiros registros do nome Peverell estão presentes neste conto de Beedle. Acerca da veracidade do conto, muitos duvidam do incrível encontro com a Morte, creditando as Relíquias da Morte como apenas grandes criações dos irmãos.Com o passar do tempo, o nome Peverell foi sumindo, devido a separação dos três irmãos. Sendo que apenas é mais provável uma linhagem direta com os Peverell para as gerações posteriores a Cadmo e Ignoto. Pelo fator de transferência da Varinha das Varinhas.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"LEFONTUAR",
+      descricao:"Fundada nas proximidades dos séculos IX.Sua existencia tinha sido, aparentemente, existinta até os tempos atuais.De origem francesa, a família é conhecida por sua inteligencia, determinação e quando necessário, a coragem. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"BRADHUBER ",
+      descricao:"Uma família nobre, de puro-sangue, que muito se ouviu falar, e muito ainda ouvirá, muito.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"RUCORN",
+      descricao:"Criada em 1868 por Wursten Runcorn e por Elizabeth Runcorn, dando apenas a cada geração dois filhos, sempre cultos, sombrios, espertos e inteligentes, excepcionalmente nenhum membro da família foi de outra comunal á não ser a Sonserina, atualmente a família pertence a 6° geração, o seu membro mais velho de 54 anos Alberto Runcorn e ao seu irmão James Wolf Runcorn de 25 anos. Vivem em meio a luz e as trevas. Não tem ao certo indícios de um Runcorn que tenha se dedicado a apoiar as Trevas nem a Luz, apenas fazer oque lhe acham certo. Vivem em localidade desconhecida, e trocam constantemente de endereço.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"FONTAINE",
+      descricao:"Fundada a mais de 300 anos, a família Fontaine possui membros nobres. Gabrielle Delacour (Fontaine atualmente) se casou com Alexander Fontaine e teve com o mesmo 2 filhos: Guilhermina e Tyerri Fontaine. Possui ligações com a família Delacour e Weasley.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"WAINWRIGHT",
+      descricao:"A família Wainwright foi construída por Rodolfo Wainwright e sua mulher Cecília, que ambos já estão mortos, mas deixaram um herdeiro, Boyd Wainwright que fundou a mansão Wainwright em Godric's Hollow. Os Wainwright são conhecidos por seu sangue-puro e por sua nobreza que prevalecem. Eles continuam seus estudos em Hogwarts, onde todos os membros caem na sonserina. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"BKSTONE",
+      descricao:"Uma Família que mora numa pequena casa em Hogsmeade, mudou-se a pouco tempo.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"FINNIGAN",
+      descricao:"Os Finnigan é uma família simples, todos de bom coração , eles são muito honestos, todos lutam pelo oque desejam, composta por bruxos e mestiço.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"VETTRA",
+      descricao:"Do sangue do dragão, descendentes dos Targaryen - um clã à muito extinto. Exibem sua herança em uma beleza notável (alguns diriam até inumana) com olhos lilases, índigos ou violeta e cabelos ouro-prateados ou branco platinados.São conhecidos como Senhores dos Dragões e parecem ser mais chegados a dragões do que qualquer outro homem, fazendo proezas com tais criaturas que nenhum outro bruxo já ousou tentar. A fim de preservar o sangue e mantê-lo puro, a Família Vettra seguiu com frequência o costume Targaryen de praticar um ato horrível: O incesto. Casando irmãos com irmãs, ou primos.Italianos de sangue puro, levam suas heranças e costumes a sério. O lema representa tudo o que eles são: Fogo & Sangue! ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"TIXIMIACE",
+      descricao:"A Família Tiximiac, uma família tão antiga quanto se possa imaginar, desde a época de Godric Gryffindor, Helga Hufflepuff, Rowena Ravenclaw e Salazar Slytherin. Uma família impressionante e também de tamanha união. Seus membros sempre foram bruxos malvados assim como poderosos também. Uma família muitíssimo rica, tendo, desde a antiguidade, milhares e milhares de Galeões, assim como taças, objetos de EXTREMO valor e outros EXTREMAMENTE Perigosos, além de sua extrema fortuna.Uma família inteiramente sangue-puro, desde as suas origens.A família é conhecida por muitos bruxos, praticamente todos do mundo bruxo, pois seu Primeiro Lorde é Aro Tiximiace, Lorde das Trevas. A família tem uma bela mansão no interior de uma floresta mágica desconhecida, em uma área inexplorada da Escócia.A Família Tiximiace, além de ser uma família, é um clã vamírico, ou seja, os membros não são parentos consanguineos, pois Aro foi transformando todos ao longo dos séculos. Uma família sinistra, calada, maligna e misteriosa, todos nela são adultos e são do mal. Esta é a Família Tiximiace, e eles tem um lema: " Os Tiximiace Nunca Dão Uma Segunda Chance ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"MCLAGGEN",
+      descricao:"A família McLaggen é bem antiga e possui sangue-puro como sempre divulga. São simples e muito modestos embora sua descendência permita luxos. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"TAYLOR",
+      descricao:"A família Taylor é uma família recente e de Sangue Puro, porém misteriosa. Eles prezam a coragem e a bondade, acreditam um dia fazer a diferença no mundo bruxo. Seu fundador ja foi um dia um Auror muito importante, mas por motivos não revelados, se demitiu.  ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"GOYLE",
+      descricao:"Goyle é uma família de poderosos e surpreendes alunos,de Hogwarts, uma família com educação e pratica nos feitiços ,pois ela cotem sangue puro,e uma família de verdadeiros feiticeiros, que cotem sempre a simplicidade e humildade,uma família por descendência de sangue puro a anos,um família sempre em harmonia e feliz.   
+      ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"SLUGHORN",
+      descricao:"Família de Horácio Slughorn, um professor de Hogwarts, que deixou-se levar pelo garoto Tom, Tom Riddle. ( Lord Voldemort )Horácio foi quem contou ao garoto sobre as Horcruxes. Então Horácio resolveu se esconder . Tempos depois retorna a Hogwarts , fazendo o Jovem Potter conseguir a “ Memória Modificada “ de Horácio. Logo Horácio viu o valor de Potter revelando a memória. Que possibilitou descobrir as fontes de Lord Voldemort , graças a Horácio as Horcruxes foram destruídas e Lord Voldemort morto. Hoje a Família de Horácio vive em sua mansão em harmonia, e ele tenta esquecer os erros do passado. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"EDWARD'S",
+      descricao:" Família originada na Inglaterra, foi criada pelos patriarcas Robert Edward's e Lucy Wildsmith, e assim a família se criou, e agora está nas mãos de Katharyna Edward's, seus membros são da sonserina, muitos adultos da família são bruxos das trevas.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"ALANCOR",
+      descricao:"A familia Alancor foi fundada á centenas de anos atrás, apartir da união do casal bruxo Jorge Follow Alancor e Matilda Alancor, que se conheceram ainda jovens e se apaixonaram a primeira vista. Honrraram o nome da familia de bondade e humildade,e souberam conservar suas heranças, proporcionando conforto e riqueza aos seus descentes.  ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"WARDOLF",
+      descricao:"Esta antiga Família, fundada nos arredores de uma Floresta por dois Bruxos muito inteligentes e ambiciosos, um casal que vivera em meados do Século XVIII, James Waldorf e a sua querida esposa Madelyn Waldorf, ambos estudaram na esplendorosa Escola de Magia e Bruxaria de Hogwarts. James, herdou misteriosamente uma grande fortuna, construindo uma Grande Mansão, que agora é o Lar da Família. Juntos tiveram três filhos, que por sua vez, expandiram a Família, dando origem à grande Família de hoje em dia, uma das mais poderosas, das mais astutas e com os Bruxos mais inteligentes desta Sociedade. Atualmente, Daeron Walford é o Chefe de Família. Os elementos desta família têm a tradição de entrar nas Casas de Gryffindor e de Ravenclaw. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"LEKSZYCKI",
+      descricao:"Família que tem um ínicio desconhecido por ser uma família milenar, sabe-se que é originária da Rússia e nos meados do século XVII migrou para a Inglaterra com o intuito de virar uma grande potência.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"SNAPE",
+      descricao:"Snape é o sobrenome de uma família com dois membros trouxas e mágicos. Em tempos, residiam numa casa na Rua da Fiação onde o Sr. e a Sra. Evans e as suas filhas, Petúnia e Lily, moravam.Algum tempo antes de 1960, a bruxa de puro-sangue, Eileen Prince, casou com um trouxa, Tobias Snape. Eles tiveram um filho, meio-sangue, Severo Snape. O casamento parecia ser infeliz, vivendo na miséria, argumentado com frequência, e tratando mal o seu filho. Os Snape foram enriquecendo ao longo dos tempos e actualmente vivem numa mansão. São da casa da Sonserina e muito bons em Poções e em Defesa Contra as Artes das Trevas.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"GRANGER",
+      descricao:"A familia Granger, é uma familia não muito conhecia pelo mundo bruxo, seu último membro bruxo, foi Hector Dagworth-Granger, um brilhante bruxo de sua época, os anos se passaram e a familia teve uma grande jornada de trouxas.. até agora. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"TOOD",
+      descricao:"Uma família de velhas tradições, leal, de alta qualidade de vida, todos de puro-sangue, vivem harmoniosamente bem na mansão em que vivem, umas das características marcantes, são seus aspectos, morais e na coragem, são quase todos os membros Sonserinos, fundada por Sweeney Tood, a não muitos anos a Família, porém a família não aceitam 'sangues-ruins', a família enfrentou bastante dificuldades na guerra e desde então não possui os mesmos aspectos de sempre, após os pais de Sweeney morrerem. ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"NOTT",
+      descricao:"Uma família de puro sangue em que é leal a todos. Uma das pessoas que se destacam nessa família é Johnatan Nott , Theodore Nott, Afinda Bagnold Nott e Andrew Nott. Todos foram da casa da Sonserina em Hogwarts menos Andrew que ainda é novo.  ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"LOVEGOOD",
+      descricao:"Uma das famílias mais antiga, mas também das mais mistriusas do mundo Bruxo. Inicio-se por volta do seculo XVI, com um casal de Sangue-Puro, á qual pertenceram á casa de Corvinal, mas a sua intêligencia não se ligava á noção da fisica ou logica, mas sim á da natureza. Durante anos esta família teve poucos membros, porém todos eles eram bondosos, amaveis, carrinhosos, e inteligentes. Passando dos estes pela Corvinal. A casa dos Lovegood passou por muitas gerações até chegar a Xenofílio Lovegood e sua esposa Ingrid Lovegood ( já falecida ). A casa é localizada perto de Ottery St. Catchpole em Devon, Inglaterra, portanto perto das casas das famílias Weasley, Fawcett e Diggory. Os Lovegood foram geralmente considerado como um família anormal pela sociedade, como eles tendem a segurar e abraçar a crença em coisas que a maioria outros não acreditam, muitas vezes publicado na revista da família, O Pasquim . A família juntou-se á família Scamander, devido ao casamento de Luna Lovegood com Rodolfo Scamander e que tiveram dois filhos, Lorcan e Lysander Scamander.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"LONGBOTTOM",
+      descricao:"Uma família muito antiga, tem sangue apenas mágico desde sempre, porém preza por humildade e nobreza, seu membro mais conhecido hoje em dia talvez seja Neville Longbottom.  ",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"MACMILLAN",
+      descricao:"Os Macmillan são uma antiga e tradicional família de sangue-puro no mundo bruxo,a mais famosa união entre famílias teve-se quando Ernesto se casou com Sally Smith, o que fez de suas futuras gerações, descendentes de Helga Hufflepuff.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"LESTRANGE",
+      descricao:"A Familia Lestrange foi criada há 200 anos por Frederich Lestrange Royal, um homem severo conhecedor de magia das trevas. Os Lestrange são de uma nobreza incrivel,com seu puro sangue. E somente alguns se adquirem o uso dela com a junção de maldade.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"BIENVOUR",
+      descricao:"Familia de origem francesa,conhecidos como trabalhadores exemplares e bruxos de fina categoria,tem curriculo em Beauxbatons como os melhores alunos das casas a humildade é sua melhor caracteristicas sendo famosos e populares por serem os  ajudantes do povo.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"Crawford",
+      descricao:"Uma família mestiça de classe média que ao longo dos tempos tem alargado os seus horizontes. A sua história remota á mais de 50 anos, quando o bruxo Edgar Crawford se apaixonou por uma trouxa de nome Cornelia e acabando por se casar com a mesma. Do seu amor nasceram duas crianças, Charlie e Katherine, agora também com os seus próprios filhos. Os membros desta família são reconhecidos principalmente pela sua determinação, humildade e, em alguns casos, astúcia.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"GAUNT",
+      descricao:"A família Gaunt é uma família-bruxa descendente de Salazar Slytherin. Morfino e Marvolo tinham orgulho de serem sangue-puros e descendente de Slytherin e eram totalmente contra Trouxas, a filha de Marvolo, Mérope, apaixonou-se por um trouxa, chamado Tom Riddle. Seu pai, Marvolo, e seu irmão, Morfino, foram inteiramente contra isso. Até que um dia, Morfino foi acusado pelo Ministério da Magia de lançar um feitiço em um trouxa, então, um funcionário do ministério, Beto Ogden, foi na casa dele. Morfino ficou bravo com a atitude de Beto e eles começaram a duelar, o funcionário fugiu e voltou com um bando de aurores que levaram Marvolo e Morfino para Azkaban. Com seu pai e seu irmão fora do caminho, Mérope enfeitiçou o Tom para amá-la, fugiu de casa e casou-se com o ele.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"DURWARD",
+      descricao:"Familia fundada no século XIII, nos arredores de Manchester, por Gregory Durward, um bruxo das trevas do passado. Era um dos mais sanguinários seguidores do Lorde das Trevas da época. Era reconhecido na sua época por ser quase um guardião do Lord. Porém Gregory, pouco após de estabelecer famiília, foi capturado pelo regime em vigor e posteriormente executado, de forma a servir como exemplo a todos os trevistas que ousavam erguer-se. Ao longo dos anos vindo a ser perseguida, a familia foi mudando sua localização geográfica, passando por vários países diferentes europeus, como Itáia, Noruega, Suécia, França e até Portugal. Ao longo desse tempo de fuga exaustiva, a familia foi mudando sua ideologia puramente trevista, incitada, cultivada por Gregory, sua mulher, e seus 2 filhos. Assim sendo, o Regime decidira que não havia mais razão para perseguir a familia. Esta então pode regressar a Londres e lá se fixou até aos dias de hoje.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"DELACOUR",
+      descricao:"Existem três coroas: a coroa da sabedoria, a coroa do sacerdócio e a coroa da realeza. Mas tendo em mãos elas, a coroa de uma boa reputação excede todas essas.''Hierarquia de herança francesa, com o passar dos anos a riqueza e influência daqueles chamados de Delacour cresceu ainda mais, alcançando o auge. Sendo a família mais vangloriada da França, tem um forte elo de cobiça por seu sangue apreciado diante de sua Veela-Herança e pureza.",
+      brasao:brasao:"../img/familias/"
+   },
+   {
+      nome:"AINGREMONT",
+      descricao:"A Família Aingremont é uma família que preza a inteligência e a esperança do bruxo. Fundada por Frêderich Aingremont Primeiro, junto de sua mulher Victorie criaram um verdadeiro conjunto de sangue-puros. Hoje possuem uma  grandiosa e situada mansão em Godric's Hollow e seu único herdeiro é Dimitre Riward Aingremont .",
+      brasao:brasao:"../img/familias/"
+   },
+
 ]
